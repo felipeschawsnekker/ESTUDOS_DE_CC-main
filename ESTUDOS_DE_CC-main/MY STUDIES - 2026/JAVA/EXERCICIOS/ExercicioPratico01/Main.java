827a0c197ebc076b12f1c18c912564ab51d9b108
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-03 21:42:51 -0330

    Update Main.java

--- ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/ExercicioPratico01/Main.java
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/ExercicioPratico01/Main.java
@@ -4,18 +4,27 @@ public class Main{
     public static void main(String[] args){
         Scanner entrada= new Scanner(System.in);
         Carro c1 = new Carro();
+        Carro c2 = new Carro();
         
-        System.out.println("Modelo: ");
+        System.out.print("Modelo 1: ");
         c1.modelo=entrada.nextLine();
+        System.out.print("Modelo 2: ");
+        c2.modelo=entrada.nextLine();
         
-        System.out.println("Cor: ");
+        System.out.print("Cor 1: ");
         c1.cor=entrada.nextLine();
+        System.out.print("Cor 2: ");
+        c2.cor=entrada.nextLine();
         
-        System.out.println("Placa: ");
+        System.out.print("Placa 1: ");
         c1.placa=entrada.nextLine();
+        System.out.print("Placa 2: ");
+        c2.placa=entrada.nextLine();
         
-        System.out.println("Ano: ");
+        System.out.print("Ano 1: ");
         c1.ano=entrada.nextInt();
+        System.out.print("Ano 2: ");
+        c2.ano=entrada.nextInt();
         
         c1.Estacionar_Carro();
         
@@ -24,6 +33,18 @@ public class Main{
         
         c1.Sair_Carro(horas);
         
+        System.out.println("Carro: " + c1.modelo);
+        System.out.println("Cor: " + c1.cor);                  
+        System.out.println("Placa: " + c1.placa);
+        System.out.println("Ano: " + c1.ano);
+        System.out.println("Tempo: " + c1.tempo_estacionado);
+        
+         System.out.println("Carro: " + c2.modelo);
+        System.out.println("Cor: " + c2.cor);                  
+        System.out.println("Placa: " + c2.placa);
+        System.out.println("Ano: " + c2.ano);
+        System.out.println("Tempo: " + c2.tempo_estacionado);
+   
         entrada.close();
     }
 }
