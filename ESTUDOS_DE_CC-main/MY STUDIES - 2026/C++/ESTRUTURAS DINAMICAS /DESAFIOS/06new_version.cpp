7613a9df01e7e74aff9ec8231f6e01ac9577d34b
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-11 20:27:39 -0300

    Update 06new_version.cpp

--- ESTUDOS_DE_CC-main/MY STUDIES - 2026/C++/ESTRUTURAS DINAMICAS /DESAFIOS/06new_version.cpp
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/C++/ESTRUTURAS DINAMICAS /DESAFIOS/06new_version.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+using namespace std;
+
+struct Aluno{
+    float n1;
+    float n2;
+    float media;
+};
+
+int main(){
+    int n;
+    cout<<"Numero de alunos: ";
+    cin>>n;
+
+    Aluno *a = new Aluno[n];
+
+    for(int i=0;i<n;i++){
+        cout<<"Aluno "<<i+1<<endl;
+        cin>>a[i].n1;
+        cin>>a[i].n2;
+
+        a[i].media = (a[i].n1 + a[i].n2)/2;
+    }
+
+    cout<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<"Media do aluno "<<i+1<<": "<<a[i].media<<endl;
+    }
+
+    delete[] a;
+}
