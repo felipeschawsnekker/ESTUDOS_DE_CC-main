24db4745b2e990a88ceb8aec48ddfe06bb923902
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-11 22:15:52 -0300

    Update Main.java

--- ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/Exercicio06/Main.java
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/Exercicio06/Main.java
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 
 /*Exercício
 
@@ -33,7 +26,7 @@ desconto não seja negativo e não ultrapasse o limite de R$ 50,00.
 através dos métodos registrarEntrada e finalizarHospedagem.
 public class Main
 */
-public class Principal {
+public class Main {
     public static void main(String[] args) {
         
         // Definimos as variaveis de preco da tabela do estacionamento
