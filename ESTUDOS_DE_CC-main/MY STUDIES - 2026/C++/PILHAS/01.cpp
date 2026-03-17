37fe18c81edd7c32e9145d07b0f0a5486918eec7
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-16 21:44:03 -0300

    Create 01.cpp

---
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/C++/PILHAS/01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#include <string>
+#include <stack>
+#include <cctype>
+int main(){
+    
+    stack<float> pilha;
+    int n;
+    
+    
+    cout<<"Quantos elementos deseja ter na sua pilha? ";
+    cin>>n;
+    float V[n]={};
+    cout<<"Digite os elementos: ";
+    for(int i=0; i<n; i++){
+        cin>>V[i];
+    }
+    
+}
