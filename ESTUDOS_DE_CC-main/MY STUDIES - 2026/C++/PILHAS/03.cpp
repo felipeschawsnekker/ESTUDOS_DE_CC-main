cf638e53713aee20046794b662b69ea0db095dd6
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-16 21:27:23 -0300

    Implement stack transfer and display elements
    Transfer elements from stack S1 to stack S2 and print them.

--- ESTUDOS_DE_CC-main/MY STUDIES - 2026/C++/PILHAS/03.cpp
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/C++/PILHAS/03.cpp
@@ -1,1 +1,26 @@
 //Transfira os elementos de uma pilha S1 para uma pilha S2
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+int main(){
+    stack<int> s1,s2;
+    int V[] = {10, 50, 40, 30, 21, -90, 50, 65, 12, 55};
+    int n= sizeof(V)/sizeof(V[0]);
+    
+    for(int i=0; i<n; i++){
+        s1.push(V[i]);
+    }
+    
+    while(!s1.empty()){
+        s2.push(s1.top());
+        s1.pop();
+        
+    }
+    
+    while(!s2.empty()){
+        cout<<s2.top()<<" ";
+        s2.pop();
+    }
+}
