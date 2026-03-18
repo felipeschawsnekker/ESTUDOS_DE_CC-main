49cbc708b548a1b757a7f08aef2d776346e3204c
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-17 22:00:35 -0300

    Create Animal.java

---
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/HERANCA/OVERRIDE/Animal/Animal.java
@@ -0,0 +1,11 @@
+class Animal{
+    void fazerSom(){
+        System.out.println("Béeeeeeeeee");
+    }
+}
+    class Gato extends Animal{
+        @Override
+        void fazerSom(){
+            System.out.println("Muuuuuuuuuu");
+        }
+    }
