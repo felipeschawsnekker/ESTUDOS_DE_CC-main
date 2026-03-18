babdb07a0259fb2e3e992ff602f479f0465466a7
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-17 21:31:53 -0300

    Create Animal.java

---
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/HERANCA/EXTENDS/Animal/Animal.java
@@ -0,0 +1,11 @@
+class Animal {
+    void fazerSom() {
+        System.out.println("Som genérico");
+    }
+}
+
+class Cachorro extends Animal {
+    void latir() {
+        System.out.println("Au au");
+    }
+}
