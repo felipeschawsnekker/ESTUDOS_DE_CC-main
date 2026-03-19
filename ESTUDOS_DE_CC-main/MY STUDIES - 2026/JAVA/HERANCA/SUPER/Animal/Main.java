9c1ce6066a8092442ff1aaa25ffd7ea2eaa697e7
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-18 21:21:41 -0300

    Create Main.java

---
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/HERANCA/SUPER/Animal/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criamos o cachorro passando o nome "Rex"
+        Cachorro meuPet = new Cachorro("Rex");
+
+        // Chamamos o método que usa o super
+        meuPet.comer();
+    }
+}
