0ad0f763e5b25b7cff57e8f9b410a42c09cde893
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-23 23:22:12 -0230

    Atualizar o Main.java

--- ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/Exercicio09/Main.java
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/Exercicio09/Main.java
@@ -1,1 +1,16 @@
-1
+public class Main {
+    public static void main(String[] args) {
+
+        Veiculo carro = new Carro("SUV Jeep", "ABC-1234", 20, 5);
+        carro.registrarEntrada(13);
+        carro.finalizarHospedagem(16);
+        carro.imprimirRelatorio();
+
+        Veiculo moto = new Moto("Honda CB500", "XYZ-9999", 10, 2);
+        moto.registrarEntrada(10);
+        moto.finalizarHospedagem(11);
+        moto.imprimirRelatorio();
+
+        carro.finalizarHospedagem(20);
+    }
+}
