413f42fd418579d101ba730f6b04f8b5d9268f43
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-23 23:33:19 -0230

    Criar Main.java

---
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/Exercicio07/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Carro c1 = new Carro("Civic", "ABC-1234");
+        c1.imprimir();
+
+        Moto m1 = new Moto("XYZ-9999");
+        m1.imprimir();
+    }
+}
