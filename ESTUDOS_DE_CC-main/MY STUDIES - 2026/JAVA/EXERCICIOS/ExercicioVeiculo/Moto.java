2ca72eb73a4aaea65556c3d7f87534b3b2b68460
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-24 21:55:31 -0300

    Create Moto.java

---
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/ExercicioVeiculo/Moto.java
@@ -0,0 +1,7 @@
+public class Moto extends Veiculo{
+    public Moto(String modelo, String placa, String cor, int ano,int temp){
+        super(modelo, placa, cor, ano, temp);
+        this.taxa=2;
+    }
+
+}
