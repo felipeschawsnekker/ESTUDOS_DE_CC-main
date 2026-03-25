903764a12933a3f038b0e68a80816e2606abceb0
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-24 21:55:22 -0300

    Create Carro.java

---
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/ExercicioVeiculo/Carro.java
@@ -0,0 +1,7 @@
+public class Carro extends Veiculo{
+    public Carro(String modelo, String placa, String cor, int ano,int temp){
+        super(modelo, placa, cor, ano, temp);
+        this.taxa=5;
+    }
+
+}
