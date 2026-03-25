86b55601412b3d2c1c63c1c53f7ad51afbbd028e
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-24 21:58:02 -0300

    Update Main.java

--- ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/ExercicioVeiculo/Main.java
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/ExercicioVeiculo/Main.java
@@ -2,7 +2,7 @@
 public class Main
 {
 	public static void main(String[] args) {
-	    //String modelo; String placa; String cor; int ano;
+	    //String modelo; String placa; String cor; int ano, int tempo;
 		Carro c = new Carro("Civic", "ABC-1234", "Preto", 2022, 12);
         Moto m = new Moto("CG 160", "XYZ-9999", "Vermelha", 2021, 6);
 
