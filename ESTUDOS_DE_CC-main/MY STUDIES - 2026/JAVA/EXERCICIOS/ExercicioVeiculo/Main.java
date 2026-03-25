eaaea6410a25910f415b66ba3678f05f2d9b04c7
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-24 21:55:12 -0300

    Create Main.java

---
+++ ESTUDOS_DE_CC-main/MY STUDIES - 2026/JAVA/EXERCICIOS/ExercicioVeiculo/Main.java
@@ -0,0 +1,15 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    //String modelo; String placa; String cor; int ano;
+		Carro c = new Carro("Civic", "ABC-1234", "Preto", 2022, 12);
+        Moto m = new Moto("CG 160", "XYZ-9999", "Vermelha", 2021, 6);
+
+        System.out.println("Carro: R$ " + c.Estacionamento());
+        System.out.println("Moto: R$ " + m.Estacionamento());
+		
+		
+
+	}
+}
