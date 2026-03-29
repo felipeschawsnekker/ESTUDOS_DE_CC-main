ef56f7025a8cb09b3d7b44f77145af3536c88def
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-29 04:43:54 +0000

    oi

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/EXERCICIOS/PrototipoEstacionamento09/model/Carro.java
@@ -0,0 +1,21 @@
+package model;
+
+
+
+
+public class Carro extends Veiculo{
+
+    public Carro(String modelo, String placa){
+        super(modelo,placa);
+    }
+    @Override
+    public int getValorPorHora(){
+        if(getFidelizado()==true){
+        System.out.println("Veiculo fidelizado!");
+        return 4;
+    }
+        else{
+            return 5;
+        }
+}
+}
