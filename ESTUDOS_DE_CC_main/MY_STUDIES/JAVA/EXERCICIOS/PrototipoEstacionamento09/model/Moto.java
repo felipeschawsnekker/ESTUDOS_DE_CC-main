e7fa11dc40dcea355b5af42b0098873d04b8651e
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-03-29 04:44:59 +0000

    oi

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES/JAVA/EXERCICIOS/PrototipoEstacionamento09/model/Moto.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Moto extends Veiculo{
+
+    public Moto(String modelo, String placa){
+        super(modelo,placa);
+    }
+    @Override
+    public int getValorPorHora(){
+        if(getFidelizado()==true){
+            System.out.println("Veiculo fidelizado!");
+        return 2;
+        }
+        else{
+            return 3;
+        }
+    }
+}
