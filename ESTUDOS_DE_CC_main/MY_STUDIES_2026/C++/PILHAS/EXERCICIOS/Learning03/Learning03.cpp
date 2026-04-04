324305275790add24e6db32b144ca9f5952b6582
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-04-04 11:48:00 +0000

    studying c++

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/PILHAS/EXERCICIOS/Learning03/Learning03.cpp
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/PILHAS/EXERCICIOS/Learning03/Learning03.cpp
@@ -3,62 +3,79 @@ Dado um array de 10 inteiros, use uma pilha para deixar o array ao contrário.*/
 
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
-
 struct Pilha{
-    int dados[10];
     int topo;
+    int dados[10];
 };
 
 void init(Pilha &p){
     p.topo=-1;
 }
-
-void push(Pilha &p,int x){
+void push(Pilha &p, int x){
     if(p.topo==9){
-        cout<<"Pilha cheia\n";
+        cout<<"Pilha cheia"<<endl;
         return;
     }
     p.dados[++p.topo]=x;
 }
 
-int pop(Pilha &p){
-    if(p.topo==-1){
-        cout<<"Pilha vazia\n";
-        return -1;
-    }
-    return p.dados[p.topo--];
-}
-
 void show(Pilha &p){
     if(p.topo==-1){
-        cout<<"Pilha vazia\n";
+        cout<<"Pilha vazia"<<endl;
         return;
     }
-    cout<<"Elementos da pilha: ";
     for(int i=p.topo; i>=0; i--){
         cout<<p.dados[i]<<" ";
     }
     cout<<endl;
 }
 
-int main(){
-    Pilha p;
-    init(p);
-    int arr[10];
-    cout<<"Digite 10 números inteiros:\n";
-    for(int i=0; i<10; i++){
-        cin>>arr[i];
-        push(p, arr[i]);
+void showarray(int *ptr,int x){
+    if(ptr==nullptr){
+        cout<<"Ponteiro nulo"<<endl;
+        return;
     }
-
-    cout<<"\nArray invertido:\n";
-    for(int i=0; i<10; i++){
-        arr[i] = pop(p);
-        cout<<arr[i]<<" ";
+    for(int i=0; i<x; i++){
+        cout<<*(ptr+i)<<" ";
     }
     cout<<endl;
-
-    return 0;
 }
 
+int pop(Pilha &p){
+    if(p.topo==-1){
+        cout<<"Pilha vazia"<<endl;
+        return -1;
+    }
+    return p.dados[p.topo--];
+}
+
+
+
+int main(){
+    int arr[10];
+    int *ptr=arr;
+    srand(time(0));
+    for(int i=0; i<10; i++){
+        int num=(rand()%50)+1;
+        *(ptr+i)=num;
+    }
+    cout<<"Array Original: ";
+    showarray(ptr,10);
+    Pilha p;
+    init(p);
+
+    for(int i=0; i<10; i++){
+        push(p,*(ptr+i));
+    }
+
+    for(int i=0; i<10; i++){
+        *(ptr+i)=pop(p);
+    }
+    cout<<"Array Invertido: ";
+    showarray(ptr,10);
+    cout<<endl;
+    return 0;
+
+}
