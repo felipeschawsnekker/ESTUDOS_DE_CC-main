6db64f377060a4caf23eb9db2e2ec9a70ae1333e
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-04-08 20:13:14 -0300

    Update DesafioProva.cpp

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/ESTRUTURAS_DINAMICAS /DESAFIOS/DesafioProva.cpp
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/ESTRUTURAS_DINAMICAS /DESAFIOS/DesafioProva.cpp
@@ -1,79 +1,83 @@
 #include <iostream>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
-
-struct tam(){
+struct tam {
     int x; 
     int y;
-}
-//pA[i*a[1] + j]
-void multiplicaMatriz(float *a, tam A, float *b, tam B,float *C){
-    for(int i=0; i<A.x; i++){
-        for(int j=0; j<B.y; j++){
-            C[i*B.y+j]=0;
-            for(int k=0; k<A.y; k++){
-                C[i*B.y+j]=(A[i*A.y+k])*(B[k*B.y+j]);
+};
+
+void multiplicaMatriz(float *A, tam Ta, float *B, tam Tb, float *C){
+    for(int i=0; i<Ta.x; i++){
+        for(int j=0; j<Tb.y; j++){
+            C[i*Tb.y+j]=0;
+            for(int k=0; k<Ta.y; k++){
+                C[i*Tb.y+j] += A[i*Ta.y+k] * B[k*Tb.y+j];
             }
         }
     }
 }
 
-void mostrarMatriz(float *p,int tam L){
-
-    for(int i=0; i<(T.x); i++){
-        cout<<"[ ";
-        for(int j=0; j<(T.y); i++){
-            cout<<p[i*T.y+j];
+void mostrarMatriz(float *p, tam T){
+    for(int i=0; i<T.x; i++){
+        cout << "[ ";
+        for(int j=0; j<T.y; j++){
+            cout << p[i*T.y+j] << " ";
         }
-        cout<<" ]"
-        cout<<endl;
+        cout << "]" << endl;
     }
 }
 
-preencherMatriz(float *p){
-    for(int i=0; i<)
+void preencherMatriz(float *p, tam T){
+    for(int i=0; i<T.x; i++){
+        for(int j=0; j<T.y; j++){
+            p[i*T.y+j] = rand()%100 + 1;
+        }
+    }
 }
+
 int main(){
-    cout<<"Digite o tamanho da matriz A: "<<endl;
-    int a[2];
-    cout<<"A[][]: "; 
-    cin>>a[0];
-    cout<<"A["<<a[0]<<"][]: ";
-    cin>>a[1];
-    cout<<"A["<<a[0]<<"]["<<a[1]<<"]"<<endl;
-    tam A;
-    A.x=a[0];
-    A.y=a[1];
-    
-    cout<<"Digite o tamanho da matriz B: "<<endl;
-    int b[2];
-    cout<<"B[][]: "; 
-    cin>>b[0];
-    cout<<"B["<<b[0]<<"][]: ";
-    cin>>b[1];
-    cout<<"B["<<b[0]<<"]["<<b[1]<<"]"<<endl;
-    tam B;
-    B.x=b[0];
-    B.y=b[1];
-    
-    tam C={A.x,B.y}
-    if(!((a[0] == b[0] && a[1]==b[1] )|| a[1]==b[0])){
-        cout<<"Tamanho inválido para multiplicação"<<endl;
+    srand(time(0));
+
+    int a[2], b[2];
+
+    cout << "Digite linhas e colunas da matriz A: ";
+    cin >> a[0] >> a[1];
+    tam A = {a[0], a[1]};
+
+    cout << "Digite linhas e colunas da matriz B: ";
+    cin >> b[0] >> b[1];
+    tam B = {b[0], b[1]};
+
+    if(A.y != B.x){
+        cout << "Tamanho inválido para multiplicação!" << endl;
         return 0;
     }
-    
-    //float *a, tam A, float *b, tam B,float *C
-    multiplicaMatriz(p)
-    
-    float matrizA[a[0]][a[1]];
-    float matrizB[b[0]][b[1]];
-    float matrizC[a[0]][b[1]];
-    float *pA=&matrizA[0][0]; // pA[i*a[1] + j]
-    float *pB=&matrizB[0][0]; // pB [i*b[1]+ j]
-    float *pC=&matriz[0][0];
-    
-    
-}
 
+    tam C = {A.x, B.y};
+
+    float matrizA[A.x][A.y];
+    float matrizB[B.x][B.y];
+    float matrizC[C.x][C.y];
+
+    float *pA = &matrizA[0][0];
+    float *pB = &matrizB[0][0];
+    float *pC = &matrizC[0][0];
+
+    preencherMatriz(pA, A);
+    preencherMatriz(pB, B);
+
+    cout << "\nMatriz A:\n";
+    mostrarMatriz(pA, A);
+
+    cout << "\nMatriz B:\n";
+    mostrarMatriz(pB, B);
+
+    multiplicaMatriz(pA, A, pB, B, pC);
+
+    cout << "\nMatriz Resultante (A x B):\n";
+    mostrarMatriz(pC, C);
+
+    return 0;
+}
