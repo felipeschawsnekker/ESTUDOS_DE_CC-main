2952c4f5eb819b6e9c7ae612dd620cb32a92ac00
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-04-27 23:35:09 -0300

    Create Paciente.java

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/EXERCICIOS/Trabalho/model/Paciente.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Paciente extends Pessoa {
+    private boolean temPlano;
+
+    public Paciente(String nome, boolean temPlano) {
+        super(nome);
+        this.temPlano = temPlano;
+    }
+
+    public boolean isTemPlano() { return temPlano; }
+}
