23261c78146ba6361b8e876a4bd21769d500cb6e
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-04-28 00:04:19 -0300

    Update ConsultaParticular.java

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/EXERCICIOS/Trabalho/model/ConsultaParticular.java
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/EXERCICIOS/Trabalho/model/ConsultaParticular.java
@@ -1,8 +1,9 @@
 package model;
 
 public class ConsultaParticular extends Consulta {
+
     @Override
-    public double calcularValor(Paciente p) {
-        return valorBase + 50.0; // Valor com acréscimo
+    public double calcularValor(Paciente paciente) {
+        return valorBase + 50.0; // acréscimo
     }
 }
