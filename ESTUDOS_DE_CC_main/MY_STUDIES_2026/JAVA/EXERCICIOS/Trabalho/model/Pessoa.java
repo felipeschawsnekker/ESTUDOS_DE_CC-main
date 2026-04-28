7531ac0631da33dbec763b8cfe177d65bccdfd5e
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-04-27 23:35:37 -0300

    Create Pessoa.java

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/EXERCICIOS/Trabalho/model/Pessoa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
