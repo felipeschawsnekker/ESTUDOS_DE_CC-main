5ce8f078ffb1ee9da497fbaf65412508c6220e78
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-04-27 23:33:28 -0300

    Update Main.java

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/EXERCICIOS/Trabalho/app/Main.java
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/EXERCICIOS/Trabalho/app/Main.java
@@ -1,1 +1,29 @@
+package app;
 
+import model.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando os objetos
+        Medico medico = new Medico("Dr. Silva", "Cardiologia");
+        Paciente paciente1 = new Paciente("João", true);  // Tem plano
+        Paciente paciente2 = new Paciente("Maria", false); // Não tem plano
+
+        // Criando as situações de cobrança
+        Consulta cPlano = new ConsultaPlano();
+        Consulta cParticular = new ConsultaParticular();
+
+        System.out.println("--- RECIBOS DE CONSULTA ---");
+
+        // Executando a lógica
+        System.out.println("Médico: " + medico.getNome());
+        
+        System.out.println("Paciente: " + paciente1.getNome());
+        System.out.println("Cobrança (Plano): R$ " + cPlano.calcularValor(paciente1));
+        
+        System.out.println("---------------------------");
+
+        System.out.println("Paciente: " + paciente2.getNome());
+        System.out.println("Cobrança (Particular): R$ " + cParticular.calcularValor(paciente2));
+    }
+}
