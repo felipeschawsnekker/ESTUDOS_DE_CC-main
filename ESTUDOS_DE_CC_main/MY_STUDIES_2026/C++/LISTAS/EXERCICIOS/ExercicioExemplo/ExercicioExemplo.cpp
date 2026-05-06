728a1748399bd3ed131f6e914ba3141e523685b2
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-05-06 19:36:29 -0300

    Implement list operations in ExercicioExemplo.cpp
    Added list operations to demonstrate push and pop methods.

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/LISTAS/EXERCICIOS/ExercicioExemplo/ExercicioExemplo.cpp
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/LISTAS/EXERCICIOS/ExercicioExemplo/ExercicioExemplo.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <list>
+using namespace std;
 
+int main() {
+	list<int> numero;
+
+	numero.push_back(10);
+	numero.push_back(12);
+	numero.push_back(13);
+	numero.push_front(2);
+	numero.push_front(5);
+
+	for(int n : numero) {
+		cout<<n<<" ; ";
+	}
+	cout<<endl;
+	numero.pop_back();
+	numero.pop_front();
+	for(int n : numero) {
+		cout<<n<<" ; ";
+	}
+	cout<<endl;
+
+}
