eef660d2234de8c6314554ce53e802f82504aee8
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-05-10 21:59:50 -0300

    Add files via upload

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/NETBEANS/EXERCICIOS/Calculadora/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Aplicacao;
+
+/**
+ *
+ * @author aliss
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        TelaCalculadora tela = new TelaCalculadora();
+        tela.show();
+    }
+}
