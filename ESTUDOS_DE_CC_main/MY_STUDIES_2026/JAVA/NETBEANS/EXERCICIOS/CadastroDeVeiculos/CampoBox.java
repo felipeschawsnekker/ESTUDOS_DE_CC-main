084a35438efbcaa24d75f262386549c159612989
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-05-12 22:17:45 -0300

    Add files via upload

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/NETBEANS/EXERCICIOS/CadastroDeVeiculos/CampoBox.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package campobox;
+
+/**
+ *
+ * @author unifeg
+ */
+public class CampoBox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Banana box = new Banana();
+        box.show();
+    }
+    
+}
