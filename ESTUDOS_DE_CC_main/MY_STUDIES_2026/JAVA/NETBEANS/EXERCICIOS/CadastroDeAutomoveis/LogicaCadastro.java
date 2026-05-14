7a31657de071088d8b5d5b805ef65dec60a23aa8
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-05-13 22:16:40 -0300

    Create LogicaCadastro.java

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/NETBEANS/EXERCICIOS/CadastroDeAutomoveis/LogicaCadastro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastro;
+
+/**
+ *
+ * @author unifeg
+ */
+public class LogicaCadastro {
+    public class Veiculo{
+        
+        public Veiculo(String placa, String modelo, String tamanho, String tipodeveiculo, String opcionais){
+        this.placa = placa;
+        this.modelo = modelo;
+        this.tamanho = tamanho;
+        this.tipodeveiculo = tipodeveiculo;
+        this.opcionais = opcionais
+        
+    }
+}
