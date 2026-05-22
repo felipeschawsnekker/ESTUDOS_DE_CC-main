cde7c2373d6f5e862ddeca31f8b90c0a9f5c4c95
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-05-22 19:47:18 -0300

    Update Prb01.cpp

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/LISTAS/LISTAS_CIRCULARES/EXERCICIOS/Prb01/Prb01.cpp
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/LISTAS/LISTAS_CIRCULARES/EXERCICIOS/Prb01/Prb01.cpp
@@ -1,1 +1,51 @@
+/*Faça um programa para manipular uma lista circular duplamente encadeada com os numeros: 10,20,30,15 com o esquema INICIALIZA, INSERIR, REMOVER, IMPRIMIR, ESVAZIAR*/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+struct NO{
+    int valor;
+    NO* prox;
+};
+
+NO* inicio;
+
+void Inicializar(){
+    inicio=NULL;
+}
+
+void Inserir(int x){
+    NO novo = (NO*)malloc(sizeof(NO));
+    novo->valor=x;
+    if(inicio=NULL){
+        inicio=novo;
+        novo->prox=inicio;
+    }
+    else{
+        NO* aux=inicio;
+        while (aux->prox!=inicio){
+            aux=aux->prox;
+        }
+        aux->prox=novo;
+        novo->prox=inicio;
+    }
+}
+
+void Imprimir{
+    if(inicio==NULL){
+        cout<<"Lista vazia"<<endl;
+        return;
+    }
+    
+    NO* aux=inicio;
+    do{
+        cout<<aux->valor<<" ";
+        aux=aux->prox;
+    }while(aux!=inicio);
+    cout<<endl;
+    }
+    
+void Remover(int x)
+    
 
