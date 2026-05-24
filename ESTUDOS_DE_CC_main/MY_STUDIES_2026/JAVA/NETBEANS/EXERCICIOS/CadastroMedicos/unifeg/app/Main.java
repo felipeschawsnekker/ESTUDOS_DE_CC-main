ce83b38b22f396cd291d8550c59e0d34a2efa4c5
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-05-24 19:44:15 -0300

    Add files via upload

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/JAVA/NETBEANS/EXERCICIOS/CadastroMedicos/unifeg/app/Main.java
@@ -0,0 +1,10 @@
+package unifeg.app;
+import unifeg.view.MenuFrame;
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Inicia o sistema
+        new MenuFrame().setVisible(true);
+    }
+}
