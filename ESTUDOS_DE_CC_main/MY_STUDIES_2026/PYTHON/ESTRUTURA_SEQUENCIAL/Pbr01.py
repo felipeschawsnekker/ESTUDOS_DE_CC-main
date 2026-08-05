d838da384112ebe323dfddca1755590dc775aa4e
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-08-05 20:06:43 -0300

    Add program to calculate arithmetic average of grades

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/PYTHON/ESTRUTURA_SEQUENCIAL/Pbr01.py
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/PYTHON/ESTRUTURA_SEQUENCIAL/Pbr01.py
@@ -1,1 +1,11 @@
+#Escreva um programa que dado três notas, calcule e imprima a média aritmetica
 
+def mediaAritmetica(entradaNota1,entradaNota2,entradaNota3):
+    return ((entradaNota1+entradaNota2+entradaNota3)/3)
+    
+print("Digite três notas!")
+a=float(input("Primeira nota: "))
+b=float(input("Segunda nota: "))
+c=float(input("Terceira nota: "))
+
+print("Média Aritmética das Três notas: ",mediaAritmetica(a,b,c))
