36dad74735bd879c71371a68d2d9ea80d13d34a9
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-08-11 19:22:26 -0300

    Update Pbr07.py

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/PYTHON/ESTRUTURA_DE_REPETICAO/Pbr07.py
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/PYTHON/ESTRUTURA_DE_REPETICAO/Pbr07.py
@@ -1,1 +1,12 @@
 
+#Calcular a soma de todos os numeros naturais de 1 até N 
+
+x=int(input("Digite um número natural N: "))
+soma=0
+for i in range(1,x+1):
+    if(i!=x):
+        print(i,end=" + ")
+    soma+=i
+    if(i==x):
+        print(x,end="")
+print(f" || A soma é {soma}",end="")
