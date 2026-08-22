383430d8a256a784a891bfcba9f31794e9bfd1b5
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-08-21 21:30:16 -0300

    Add function to generate list from two input lists
    Implement function to concatenate two lists and print the result.

---
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/PYTHON/LISTAS_TUPLAS/EXERCICIOS/TUPLAS/Pbr07.py
@@ -0,0 +1,11 @@
+#PBR07 INTERSEÇÃO ENTRE LISTAS
+#RECEBA DUAS LISTAS E RETORNE UMA NOVA LISTA CONTENDO OS ELEMENTOS QUE EXISTEM
+#EM AMBAS
+
+def geraLista(lista1,lista2):
+    print(lista1+lista2)
+    return (lista1+lista2)
+
+lista1=[1,2,3,4]
+lista2=[5,6,7,8]
+geraLista(lista1,lista2)
