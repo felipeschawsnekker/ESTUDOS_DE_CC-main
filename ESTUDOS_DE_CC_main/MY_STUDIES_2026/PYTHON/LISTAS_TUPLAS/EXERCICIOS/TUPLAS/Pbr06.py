86a07a1e82c81afef85f2f24b5977ded58983bf0
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-08-21 21:28:05 -0300

    Update Pbr06.py

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/PYTHON/LISTAS_TUPLAS/EXERCICIOS/TUPLAS/Pbr06.py
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/PYTHON/LISTAS_TUPLAS/EXERCICIOS/TUPLAS/Pbr06.py
@@ -1,1 +1,18 @@
+#PRB06 TUPLAS DE PARES ORDENADOS
+#Dada uma lista de número inteiros crie uma tupla com pares ordenados
+#Numero, numero ao quadrado
 
+listaInteiro=[1,2,3,4,5,6,7,8,9]
+
+def geraTupla(lista):
+    templista=[]
+    for i in lista:
+        templista.append(i**2)
+    
+    novaLista=tuple(zip(listaInteiro,templista))
+    return novaLista
+    print(novaLista)
+    
+geraTupla(listaInteiro)
+        
+        
