cb342b0b06069ab06ab12aab8d8ce2059f9ba5cf
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-08-26 19:40:08 -0300

    Update Prb04.cpp

--- ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/ARQUIVOS/EXERCICIOS/Prb04.cpp
+++ ESTUDOS_DE_CC_main/MY_STUDIES_2026/C++/ARQUIVOS/EXERCICIOS/Prb04.cpp
@@ -1,31 +1,44 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 using namespace std;
 
 void geraArquivo(){
     FILE *arquivo=fopen("Arq04.txt","w");
         if(arquivo!=nullptr){
-            cout<<"Arquivo criado!";
+            cout<<"\nArquivo criado!";
+        }
+        else{
+            cout<<"\nErro";
+            return;
         }
     fclose(arquivo);
     }
 
 void gravarArquivo(){
     FILE *arquivo=fopen("Arq04.txt","w");
-    fprintf(arquivo,"Frase qualquer, escrevendo");
-    cout<<"Arquivo escrito!"<<endl;
+    if(arquivo!=nullptr){
+            cout<<"\nArquivo aberto!";
+        }
+        else{
+            cout<<"\nErro";
+            return;
+        }
+        
+    const char *frase="\nfrase qualquer, apenas escrevendo!";
+    fwrite(frase,sizeof(char),strlen(frase),arquivo);
     fclose(arquivo);
 }
 
 void lerArquivo(){
     FILE *arquivo=fopen("Arq04.txt","r");
     if(arquivo==NULL){
-        cout<<"Erro ao abrir o arquivo!"<<endl;
+        cout<<"\nErro ao abrir o arquivo!"<<endl;
     }
     char buffer[100];
     size_t itensLidos = fread(buffer,sizeof(char),99,arquivo);
     buffer[itensLidos]='\0';
-    cout<<"Conteúdo lido: "<<buffer;
+    cout<<"\nConteúdo lido: "<<buffer;
     fclose(arquivo);
 }
 
